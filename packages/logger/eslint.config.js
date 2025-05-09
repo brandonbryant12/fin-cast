434ab71886fb3db8f0179ae4cda79ed1d9f9f46b
Brandon Bryant <brandon.bryant002@gmail.com>
2025-05-09 09:21:48 -0400

    fix lint

--- packages/logger/eslint.config.js
+++ packages/logger/eslint.config.js
@@ -2,7 +2,6 @@ import baseConfig from "@repo/eslint-config/base";
 
 /** @type {import("eslint").Linter.Config[]} */
 export default [
-  { ignores: ["dist/**"] }, // Keep ignoring potential leftover build artifacts
+  { ignores: ["dist/**"] },
   ...baseConfig,
-  // No longer need restrictEnvAccess here as the package won't access process.env
 ]; 
